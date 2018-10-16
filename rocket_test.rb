15ca0cb5e254e28d163037f7df8a607c5500e338
StephenVarela <varelast92@gmail.com>
2018-10-16 13:43:42 -0400

    added assert match to check rocket status, and assert includes to check initialize method for name and colour

--- rocket_test.rb
+++ rocket_test.rb
@@ -70,20 +70,94 @@ class RocketTest < Minitest::Test
   end
 
   def test_land_sets_flying_to_false_if_flying
-
+    #arrange
+      @flying_rocket = Rocket.new(flying: true)
+    #act
+      @flying_rocket.land
+      actual = @flying_rocket.flying?
+      expected = false
+    #assert
+      assert_equal(expected, actual)
   end
 
   def test_land_returns_false_if_not_flying
-
+    #arrange
+      @not_flying_rocket = Rocket.new(flying: false)
+    #act
+      actual =  @not_flying_rocket.land
+      expected = false
+    #assert
+      assert_equal(expected, actual)
   end
 
   def test_status_if_flying_return_message
+      @not_flying_rocket = Rocket.new(flying: false)
 
+      actual = @not_flying_rocket.status
+      exp = 'lift off!'
+      assert_match(exp, actual)
   end
 
   def test_status_if_not_flying_return_message
+    @flying_rocket = Rocket.new(flying: true)
+
+    actual = @flying_rocket.status
+    exp = 'through the sky!'
+    assert_match(exp, actual)
+  end
+
+  def test_check_name_initialize
+    @my_rocket = Rocket.new(name: 'Cool Rocket')
+
+    actual = @my_rocket.name
+    expected = 'Cool Rocket'
+
+    assert_equal(expected, actual)
+  end
+
+  def test_check_name_default
+    @my_rocket = Rocket.new
+
+    actual = @my_rocket.name
+    prefixes = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"]
+
+    suffixes = ["i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x"]
+
+    collection = []
+
+    prefixes.each do |prefix|
+      suffixes.each do |suffix|
+        collection << "#{prefix.capitalize} #{suffix.upcase}"
+      end
+    end
+
+    assert_includes(collection, actual)
+  end
+
+  def test_check_colour_initialize
+    @my_rocket = Rocket.new(colour: 'Violet')
+
+    actual = @my_rocket.colour
+    expected = 'Violet'
+
+    assert_equal(expected, actual)
+  end
+
+  def test_check_colour_default
+    @my_rocket = Rocket.new
+
+    actual = @my_rocket.colour
+    colour_list = ["red", "yellow", "blue", "green", "purple", "orange"]
+
+    assert_includes(colour_list, actual)
+  end
+
+  def test_check_flying_initialize
 
   end
 
+  def test_check_flying_default
+
+  end
 
 end
